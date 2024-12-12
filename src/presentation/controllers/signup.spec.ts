2ecbe756365454dd0b5df95af4f1b592e6232414
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2024-12-12 15:53:21 -0300

    test: retorna 400 se não enviar o nome do usuário

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from './signup';
+
+describe('Controlador de login', () => {
+  test('Deve retornar 400, se não enviar o nome do usuário', () => {});
+  const sut = new SignUpController();
+  const httpRequest = {
+    /* name: 'any_name', */
+    email: 'any_emai',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  };
+  const httpResponse = sut.handle(httpRequest);
+  expect(httpResponse.statusCode).toBe(400);
+});
