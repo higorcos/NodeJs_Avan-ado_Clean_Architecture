a699f67a1efe3aaa08c6aebea3948f55ca3e9db3
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2024-12-12 16:26:05 -0300

    feat: ajustes validação de parametros

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,12 @@
 export class SignUpController {
   handle(httpRequest: any): any {
     console.log(httpRequest);
-    return { statusCode: 400, body: new Error('parâmetro ausentes: nome') };
+
+    if (!httpRequest.body.name) {
+      return { statusCode: 400, body: new Error('parâmetro ausente: name') };
+    }
+    if (!httpRequest.body.email) {
+      return { statusCode: 400, body: new Error('parâmetro ausente: email') };
+    }
   }
 }
