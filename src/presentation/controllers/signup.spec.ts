1deec3a2389e4d50cadc5fdc2e2fe75512103b49
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2024-12-12 16:26:14 -0300

    test: ajustes validação de parametros

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,17 +1,33 @@
 import { SignUpController } from './signup';
 
 describe('Controlador de login', () => {
-  test('Deve retornar 400, se não enviar o nome do usuário', () => {});
-  const sut = new SignUpController();
-  const httpRequest = {
-    body: {
-      /* name: 'any_name', */
-      email: 'any_emai',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
-    }
-  };
-  const httpResponse = sut.handle(httpRequest);
-  expect(httpResponse.statusCode).toBe(400);
-  expect(httpResponse.body).toEqual(new Error('parâmetro ausentes: nome'));
+  test('Deve retornar 400, se não enviar o nome do usuário', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        /* name: 'any_name', */
+        email: 'any_emai',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('parâmetro ausente: name'));
+  });
+
+  test('Deve retornar 400, se não enviar o email do usuário', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        /*  email: 'any_emai', */
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('parâmetro ausente: email'));
+  });
 });
