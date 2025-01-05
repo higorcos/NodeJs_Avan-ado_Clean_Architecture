4a43069249f8596c3b28fb2ff17b90ec1d344aff
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-05 18:36:54 -0300

    test: ensure SignUpController returns 400 if no password

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,36 @@ describe('Controlador de login', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
+
+  test('Deve retornar 400, se não enviar o password do usuário', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_emai',
+        /* password: 'any_password', */
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  });
+
+  test('Deve retornar 400, se não enviar o passwordConfirmation do usuário', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_emai',
+        password: 'any_password'
+        /*  passwordConfirmation: 'any_password' */
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError('passwordConfirmation')
+    );
+  });
 });
