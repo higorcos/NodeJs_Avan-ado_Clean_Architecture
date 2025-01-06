03b03e13673679915d5f6fb989b02b11a22c55e0
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-06 13:59:06 -0300

    feat: detorno quando email for invalido

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,16 @@ import { MissingParamError } from '../error/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 import { HttpRequest, HttpResponse } from '../protocols/https';
 import { Controller } from '../protocols/controllers';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParamError } from '../error/invalid-param-error';
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(paramsEmailValidator: EmailValidator) {
+    this.emailValidator = paramsEmailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFileds = [
       'name',
@@ -16,6 +24,11 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'));
     }
   }
 }
