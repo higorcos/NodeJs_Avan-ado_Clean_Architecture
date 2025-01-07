b8fc1dc33ba1817b19718f8d58fbac2d96b4a8f6
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-07 18:03:30 -0300

    refactor: ajuste aync await  error

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '../protocols/https';
-import { ServerError } from '../error/server-error';
+import { ServerError } from '../error';
 
 export const SucessReponse = (data: any): HttpResponse => ({
   statusCode: 200,
