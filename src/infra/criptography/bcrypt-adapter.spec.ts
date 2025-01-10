3c1e4cb49b0b0d7f738a1b1b971920946e2dcc5e
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-10 20:43:16 -0300

    test: deve retornar a hash gerada

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcryt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve('hash'));
+  }
+}));
+
 describe('Bcrypt Adapter', () => {
   test('Deve chamar bcrypt com o valor correto', async () => {
     const salt = 12;
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcryt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Deve retornar a hash gerada', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hash');
   });
 });
