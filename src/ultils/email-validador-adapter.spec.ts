13b134320b4bf7f3d89d4fe2856c3a13bf2e578c
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-10 13:46:10 -0300

    test: validator de email retorno true e mock

--- src/ultils/email-validador-adapter.spec.ts
+++ src/ultils/email-validador-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from './email-validador-adapter';
+import validator from 'validator';
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true;
+  }
+}));
 
 describe('EmailValidator Adapter', () => {
   test('Retorna false se o email não for válido', () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false); //mocando uma valor
     const isValid = sut.isValid('invalid_email@gmail.com');
     expect(isValid).toBe(false);
+  });
+
+  test('Retorna true se o email for válido', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('valid_email@gmail.com');
+    expect(isValid).toBe(true);
   });
 });
