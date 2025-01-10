26632f2ac240071eb43c415e3baae1753c01e5e7
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-10 13:54:29 -0300

    refactor: tranformando email validator em makeSut

--- src/ultils/email-validador-adapter.spec.ts
+++ src/ultils/email-validador-adapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock('validator', () => ({
   }
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
+
 describe('EmailValidator Adapter', () => {
   test('Retorna false se o email não for válido', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false); //mocando uma valor
     const isValid = sut.isValid('invalid_email@gmail.com');
     expect(isValid).toBe(false);
   });
 
   test('Retorna true se o email for válido', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid('valid_email@gmail.com');
     expect(isValid).toBe(true);
   });
 
   test('Verifica se email passado é o mesmo que foi validado', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, 'isEmail');
     sut.isValid('any_email@gmail.com');
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@gmail.com');
