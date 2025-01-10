7f536d1993eb202f2e21d676be7fd3bef4459ff7
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-10 13:45:55 -0300

    feat: validator de email retorno true e mock

--- src/ultils/email-validador-adapter.ts
+++ src/ultils/email-validador-adapter.ts
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { EmailValidator } from '../presentation/protocols/email-validator';
+import validator from 'validator';
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
