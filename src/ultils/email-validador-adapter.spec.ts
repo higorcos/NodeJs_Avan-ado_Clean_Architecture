955884857a688d43e13e8a81a7515ba1407f944a
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-10 13:35:14 -0300

    test: validator de email retorno false

--- src/ultils/email-validador-adapter.spec.ts
+++ src/ultils/email-validador-adapter.spec.ts
@@ -1,3 +1,5 @@
+import { EmailValidatorAdapter } from './email-validador-adapter';
+
 describe('EmailValidator Adapter', () => {
   test('Retorna false se o email não for válido', () => {
     const sut = new EmailValidatorAdapter();
