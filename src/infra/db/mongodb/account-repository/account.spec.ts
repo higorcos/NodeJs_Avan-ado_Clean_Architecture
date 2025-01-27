c01c2e9a98bcf4c762b45f034d7a3cc954e7bf9e
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-01-27 16:11:20 -0300

    test: tá retornando um account

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '../helpers/monge-helper';
+import { AccountMongoRepository } from './account';
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -19,7 +20,7 @@ describe('Account Mongo Repository', () => {
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe('any_name');
-    expect(account.email).toBe('any_email');
+    expect(account.email).toBe('any_email@gmail.com');
     expect(account.password).toBe('any_password');
   });
 });
