1211eeed31b4baff7f9130283c09aa7627bc7cf4
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-02-04 21:07:52 -0300

    feat: conecao mongodb depois app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,11 @@
-import app from './config/app';
-const port = 3000;
-app.listen(port, () => console.log(`Server Running at port: ${port}`));
+import { MongoHelper } from '../infra/db/mongodb/helpers/monge-helper';
+import env from './config/env';
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+    app.listen(env.port, () =>
+      console.log(`Server Running at port: ${env.port}`)
+    );
+  })
+  .catch(console.error);
