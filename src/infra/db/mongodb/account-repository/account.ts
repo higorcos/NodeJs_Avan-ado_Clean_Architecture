bcd9d8be2520b1ee587758af42c223f2b14543dd
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-02-05 21:16:16 -0300

    refactor: mongo

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/monge-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    const accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.insertOne(accountData);
     return MongoHelper.map(accountData);
   }
---
+++ src/infra/db/mongodb/helpers/monge-helper.e.ts
@@ -0,0 +1,20 @@
+/* import { MongoHelper as sut } from './monge-helper';
+
+describe('MongoHelper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await sut.disconnect();
+  }); */
+/* 
+  test('should reconnect if mongodb is down', async () => {
+    Não tem necessidade de testar isso aqui o isConnect do mongo foi descontinuado 
+    let accountCollection = sut.getCollection('accounts');
+    expect(accountCollection).toBeTruthy();
+    await sut.disconnect();
+    accountCollection = sut.getCollection('accounts');
+    expect(accountCollection).toBeTruthy();
+  }); */
+/* }); */
