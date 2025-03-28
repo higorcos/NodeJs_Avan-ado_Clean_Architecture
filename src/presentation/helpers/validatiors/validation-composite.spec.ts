2241dd35098d5e141607158f6f43c42b8ba448ae
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-03-28 14:15:04 -0300

    test: should return the first error if more then one validation fails

--- src/presentation/helpers/validatiors/validation-composite.spec.ts
+++ src/presentation/helpers/validatiors/validation-composite.spec.ts
@@ -35,4 +35,14 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
   });
+  //Deve retornar o primeiro erro se mais de uma validação falhar
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut();
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error());
+    jest
+      .spyOn(validationStubs[1], 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
+    const error = sut.validate({ field: 'any_value' });
+    expect(error).toEqual(new Error());
+  });
 });
