58aa90b3ab72a17bcff66eec93e6d049ea9533f3
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-03-28 12:52:17 -0300

    refactor: move sut

--- src/presentation/helpers/validatiors/required-field-validation.spec.ts
+++ src/presentation/helpers/validatiors/required-field-validation.spec.ts
@@ -1,16 +1,18 @@
 import { MissingParamError } from '../../error';
 import { RequiredFieldValidation } from './required-field-validation';
-
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field');
+};
 describe('RequiredField Validation', () => {
   //Deve retornar um erro de parâmetro ausente se a validação falhar
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field');
+    const sut = makeSut();
     const error = sut.validate({ name: 'any_name' });
     expect(error).toEqual(new MissingParamError('field'));
   });
   //Não deve retornar se a validação for bem-sucedida
   test('Should not return if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('field');
+    const sut = makeSut();
     const error = sut.validate({ name: 'any_name' });
     expect(error).toBeFalsy();
   });
