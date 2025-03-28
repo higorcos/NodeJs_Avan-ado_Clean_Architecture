2fe343285eba29d649318a6d84c70efe76c8230e
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-03-28 14:11:39 -0300

    refactor: should return an error if any validation fails

--- src/presentation/helpers/validatiors/validation-composite.spec.ts
+++ src/presentation/helpers/validatiors/validation-composite.spec.ts
@@ -16,21 +16,21 @@ const makeValidationStub = (): Validation => {
 
 interface SutTypes {
   sut: ValidationComposite;
-  validationStub: Validation;
+  validationStubs: Validation[];
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidationStub();
-  const sut = new ValidationComposite([validationStub]);
-  return { sut, validationStub };
+  const validationStubs = [makeValidationStub(), makeValidationStub()];
+  const sut = new ValidationComposite(validationStubs);
+  return { sut, validationStubs };
 };
 
 describe('Validation Composite', () => {
   //Deve retornar um erro se alguma validação falhar
   test('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut();
+    const { sut, validationStubs } = makeSut();
     jest
-      .spyOn(validationStub, 'validate')
+      .spyOn(validationStubs[0], 'validate')
       .mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
