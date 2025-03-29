190dcff460e822bfd9f99479b2956be163b1659d
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2025-03-28 21:58:32 -0300

    feat: add Login Validation secceeds

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,12 +1,12 @@
-import { ValidationComposite } from '../../presentation/helpers/validatiors/validation-composite';
-import { makeSignUpValidation } from './signup-validation';
-import { RequiredFieldValidation } from '../../presentation/helpers/validatiors/required-field-validation';
-import { Validation } from '../../presentation/helpers/validatiors/validation';
-import { CompareFieldsValidation } from '../../presentation/helpers/validatiors/compare-field-validation';
-import { EmailValidation } from '../../presentation/helpers/validatiors/email-validation';
-import { EmailValidator } from '../../presentation/protocols/email-validator';
+import { ValidationComposite } from '../../../presentation/helpers/validatiors/validation-composite';
+import { makeSignUpValidation } from '../signup/signup-validation';
+import { RequiredFieldValidation } from '../../../presentation/helpers/validatiors/required-field-validation';
+import { Validation } from '../../../presentation/helpers/validatiors/validation';
+import { CompareFieldsValidation } from '../../../presentation/helpers/validatiors/compare-field-validation';
+import { EmailValidation } from '../../../presentation/helpers/validatiors/email-validation';
+import { EmailValidator } from '../../../presentation/protocols/email-validator';
 
-jest.mock('../../presentation/helpers/validatiors/validation-composite');
+jest.mock('../../../presentation/helpers/validatiors/validation-composite');
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
